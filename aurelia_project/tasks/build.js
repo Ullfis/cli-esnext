5336fd417928dc43beb8fc76d988c0325ede4f3a
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-10-10 09:56:26 +0200

    include requirejs and requirejs/text via NPM

--- aurelia_project/tasks/build.js
+++ aurelia_project/tasks/build.js
@@ -2,10 +2,8 @@ import gulp from 'gulp';
 
 import buildCompile from './build-compile';
 import prepareOutput from './prepare-output';
-import prepareScripts from './prepare-scripts';
 
 export default gulp.series(
   prepareOutput,
-  prepareScripts,
   buildCompile
 );
--- aurelia_project/tasks/prepare-materialize.js
+++ aurelia_project/tasks/prepare-materialize.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import merge from 'merge-stream';
-import changedInPlace from 'gulp-changed-in-place';
 import project from '../aurelia.json';
 import path from 'path';
 
@@ -9,11 +8,9 @@ export default function prepareMaterialize() {
   let source = 'node_modules/materialize-css';
 
   let taskCss = gulp.src(path.join(source, 'dist/css/materialize.min.css'))
-    .pipe(changedInPlace({firstPass:true}))
     .pipe(gulp.dest(path.join(project.platform.output, '../', 'styles')));
 
   let taskFonts = gulp.src(path.join(source, 'dist/fonts/roboto/*'))
-    .pipe(changedInPlace({firstPass:true}))
     .pipe(gulp.dest(path.join(project.platform.output, '../', 'fonts/roboto')));
 
   return merge(taskCss, taskFonts);
